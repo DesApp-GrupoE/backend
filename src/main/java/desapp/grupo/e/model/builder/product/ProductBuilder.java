d3fbfacc38df73de6d894ddcee7d94401b4a93aa
Federico Presa <fpresa@onapsis.com>
2020-05-10 21:01:35 -0300

    trying testing a the creation of builder

---
+++ src/main/java/desapp/grupo/e/model/builder/product/ProductBuilder.java
@@ -0,0 +1,64 @@
+package desapp.grupo.e.model.builder.product;
+import desapp.grupo.e.model.product.Product;
+
+
+public class ProductBuilder {
+    private String name;
+    private String brand;
+    private Double price;
+    private Integer stock;
+    private String img;
+    private Long idCommerce;
+
+    public static ProductBuilder aProduct() {
+        return new ProductBuilder();
+    }
+
+    public ProductBuilder withName(String name) {
+        this.name= name;
+        return this;
+    }
+
+    public ProductBuilder withBrand(String brand) {
+        this.brand= brand;
+        return this;
+    }
+
+    public ProductBuilder withPrice(Double price) {
+        this.price= price;
+        return this;
+    }
+
+    public ProductBuilder withStock(int stock) {
+        this.stock= stock;
+        return this;
+    }
+
+    public ProductBuilder anyProduct() {
+            this.name = "chocolate";
+            this.brand = "Milka";
+            this.price = 10.0;
+            this.stock = 50;
+            this.img = "test";
+            this.idCommerce = null;
+
+        return this;
+    }
+
+    public Product build() {
+        Product product = new Product(this.name, this.brand, this.price, 
+                                      this.stock, this.img, this.idCommerce);
+        resetBuilder();
+        return product;
+    }
+
+    public void resetBuilder() {
+        this.name = null;
+        this.brand = null;
+        this.price = null;
+        this.stock = null;
+        this.img = null;
+        this.idCommerce = null;
+    }
+
+}
--- src/test/java/desapp/grupo/e/model/product/ProductTest.java
+++ src/test/java/desapp/grupo/e/model/product/ProductTest.java
@@ -1,5 +1,5 @@
 package desapp.grupo.e.model.product;
-
+import desapp.grupo.e.model.builder.ProductBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +8,13 @@ public class ProductTest {
     @Test
     public void test() {
         Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    public void createMerchantShouldHasRoleMERCHANT_ByDefult() {
+        Product product = ProductBuilder.aProduct().anyProduct().build();
+        String productNameExpected = "test";
+
+     Assertions.assertEquals(product.getName(), productNameExpected);
     }
 }
