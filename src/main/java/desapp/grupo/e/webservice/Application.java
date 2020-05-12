708e70c07c0a01435f514988210867608b56e885
Ariel Ramirez <arielramirez20395@gmail.com>
2020-05-12 20:51:55 -0300

    Update to JPA
    * Configure database in application
    * We do that Application Spring run with JPA
    * Remove clases unused
    * Remove persistence.xml

--- src/main/java/desapp/grupo/e/webservice/Application.java
+++ src/main/java/desapp/grupo/e/webservice/Application.java
@@ -1,6 +1,7 @@
 package desapp.grupo.e.webservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import desapp.grupo.e.service.log.Log;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"desapp.grupo.e.persistence"})
+@ComponentScan(basePackages =
+        {   "desapp.grupo.e.model",
+            "desapp.grupo.e.webservice.controller",
+            "desapp.grupo.e.persistence",
+            "desapp.grupo.e.service",
+        })
+@EntityScan(basePackages = {"desapp.grupo.e.model"})
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/desapp/grupo/e/webservice/beans/ConfigBean.java
+++ src/main/java/desapp/grupo/e/webservice/beans/ConfigBean.java
@@ -5,32 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import desapp.grupo.e.service.login.LoginService;
-import desapp.grupo.e.persistence.daos.UserDao;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 @Configuration
 public class ConfigBean {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        return Persistence.createEntityManagerFactory("ComprandoEnCasa");
-    }
-
-    @Bean
-    public EntityManager entityManager() {
-        return entityManagerFactory().createEntityManager();
-    }
-
-    @Bean
-    public UserDao customerDao() {
-        return new UserDao(entityManager());
-    }
 
     @Bean
     // Este método inyecta en el código un nuevo LoginService utilizando el nombre del método
