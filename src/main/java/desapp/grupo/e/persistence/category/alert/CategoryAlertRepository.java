4ff56eb11bcdd384f485a392ed6bac735d879161
Ariel Ramirez <arielramirez20395@gmail.com>
2020-05-24 12:42:01 -0300

    Add WebSecurity to disable cors
    * Without we can't use postman
    * Remove import unused from CategoryAlertRepository

--- src/main/java/desapp/grupo/e/persistence/category/alert/CategoryAlertRepository.java
+++ src/main/java/desapp/grupo/e/persistence/category/alert/CategoryAlertRepository.java
@@ -2,7 +2,6 @@ package desapp.grupo.e.persistence.category.alert;
 
 import desapp.grupo.e.model.product.CategoryAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
